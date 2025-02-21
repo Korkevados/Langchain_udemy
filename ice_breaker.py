8fd5309717e0e5845b96e371236c8f100f318a5a
Korkevados <144181487+Korkevados@users.noreply.github.com>
2025-02-21 15:42:20 +0200

    finish uce breaker

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
+from typing import  Tuple
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_party.linkedin import scrape_linkedin_profile
-def ice_break_with(name:str)->str:
+from output_parsers import summary_parser,Summary
+
+def ice_break_with(name:str)->Tuple[Summary,str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_url)
 
@@ -11,20 +14,24 @@ def ice_break_with(name:str)->str:
         given the information {information} about a person I want you to create:
         1. A short summary
         2. two interesting facts about them
-        """
+        Use  information from Linkedin \n{format_instructions}
+    """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
 
-    res = chain.invoke(input={"information": linkedin_data})
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
-
+    return res, linkedin_data.get("photoUrl")
 
 if __name__ == "__main__":
     load_dotenv()
